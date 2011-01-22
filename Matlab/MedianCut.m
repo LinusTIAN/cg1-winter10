f777625136d51a916ffb9cb99bfb0a60ac73d83a
hanashubin <hanashubin@2ff579a8-b8b1-c11a-477f-bc6c74f83876>
2011-01-22 20:57:07 +0000

    finish(?) ans 3 with a bug

--- Matlab/MedianCut.m
+++ Matlab/MedianCut.m
@@ -1,76 +1,56 @@
-function Y = MedianCut(theImage,level) 
+function Y = MedianCut(theImage,numOfColors) 
   
 theImage = double(theImage);
+colorSize = 256*256*256+1;
+x = size(theImage,1);
+y = size(theImage,2);
 % Contains the colors in the original image
-colorSize=256*256*256+1;
 colorMap = zeros(1,colorSize); 
-maxColor=0;
-for i=1:size(theImage,1) 
-    for j=1:size(theImage,2) 
-        %count num of times each color appear
-        k=theImage(i,j); 
-        colorMap(k+1)=colorMap(k+1)+1; 
-        maxColor=max(maxColor, k+1);
+maxColor = 0;
+
+for i=1:x 
+    for j=1:y 
+        %count num of times each color appears
+        k = theImage(i,j); 
+        colorMap(k+1) = colorMap(k+1)+1; 
+        maxColor = max(maxColor, k+1);
     end 
 end 
+
   
 %make an array with all the colors 
-y=1;   
+tempCounter=1;
 for c=1:maxColor     
     if colorMap(c)==0 
         continue 
-    end 
-  
-    for x=1:colorMap(c) 
-        img(y)=c-1; 
-        y=y+1; 
+    end   
+    for ppf=1:colorMap(c) 
+        orderedColorMap(tempCounter) = c-1; 
+        tempCounter = tempCounter+1; 
     end 
 end 
+range = (tempCounter-1)/numOfColors ;
   
-n=size(img,2); 
-a=mod(n,level); 
-s=(n-a)/level; 
-  
-m=1; 
-if a~=0 
-    for i=1:a 
-        for j=1:s+1 
-            sub(j)=img(m+j-1); 
-        end 
-        m=m+s+1; 
-        maks=max(sub); 
-        mini=min(sub); 
-        ort=round(sum(sub)/(s+1)); 
-    
-        for z=1:size(theImage,1) 
-            for t=1:size(theImage,2) 
-                if (theImage(z,t)<= maks) & (theImage(z,t)>=mini) 
-                    theImage(z,t)=ort; 
-                end 
+firstColor = 1;
+%start algo
+for i=1:numOfColors
+    %all colors in range
+    for j=1:range 
+        colorsRange(j) = orderedColorMap(firstColor+j-1); 
+    end 
+    %find average color
+    maxC = max(colorsRange);
+    minC = min(colorsRange);
+    averageC = round(sum(colorsRange)/(range));
+    %change all colors in range to average color
+    for row=1:x
+        for col=1:y
+            if (maxC >= theImage(row,col)) && ( minC <= theImage(row,col)) 
+                theImage(row,col) = averageC;
             end 
         end 
-    
-    end 
+    end   
+    firstColor = firstColor+range;   
 end 
   
-for i=a+1:level 
-    for j=1:s 
-        subb(j)=img(m+j-1); 
-    end 
-    m=m+s; 
-    maks=max(subb); 
-    mini=min(subb); 
-    ort=round(sum(subb)/(s)); 
-    
-    for z=1:size(theImage,1) 
-        for t=1:size(theImage,2) 
-            if (theImage(z,t)<= maks) & (theImage(z,t)>=mini) 
-                theImage(z,t)=ort; 
-            end 
-        end 
-    end 
-    
-end 
-  
-%Y=uint8(theImage); 
-Y=theImage; 
+Y = uint32(theImage); 
